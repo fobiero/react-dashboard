bf4c80ba89a5b2836bec50e57536a3ababf9ed5a
fobiero <felbiero@gmail.com>
2022-10-26 14:09:36 +0300

    load dynamic data

--- src/components/widgets/Widget.js
+++ src/components/widgets/Widget.js
@@ -1,10 +1,66 @@
 import './widget.scss'
+import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import AccountBalanceWalletOutlinedIcon from '@mui/icons-material/AccountBalanceWalletOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import MonetizationOnOutlinedIcon from '@mui/icons-material/MonetizationOnOutlined';
 
-const Widget = () => {
+const Widget = ({type}) => {
+
+    let data;
+    const amount = 100;
+    const diff = 20;
+
+    switch(type){
+        case "user":
+            data = {
+                title: 'USERS',
+                isMoney: false,
+                link: "See all Users",
+                icon: <PersonOutlineOutlinedIcon className='icon' />
+            };
+            break;
+            case "order":
+                data = {
+                    title: 'ORDER',
+                    isMoney: false,
+                    link: "View all Orders",
+                    icon: <ShoppingCartOutlinedIcon className='icon' />
+                };
+            break;
+            case "earning":
+                data = {
+                    title: 'EARNINGS',
+                    isMoney: true,
+                    link: "View net earnings",
+                    icon: <MonetizationOnOutlinedIcon className='icon' />
+                };
+            break;
+            case "balance":
+                data = {
+                    title: 'BALANCES',
+                    isMoney: true,
+                    link: "See Details",
+                    icon: <AccountBalanceWalletOutlinedIcon className='icon' />
+                };
+            break;
+        default:
+            break;
+    }
+
     return(
         <div className='widget'>
-            <div className='left'>left</div>
-            <div className='right'>right</div>
+            <div className='left'>
+                <span className='title'>{data.title}</span>
+                <span className='counter'>{data.isMoney && "$"} {amount}</span>
+                <span className='link'>{data.link}</span>
+            </div>
+            <div className='right'>
+                <div className='percentage positive'> 
+                <KeyboardArrowUpOutlinedIcon />
+                {diff } %</div>
+                {data.icon} 
+            </div>
         </div>
     )
 }
